827ed89b67dbb60ea15f771c40aee3d982128061
Sasarino MARi <develop.mari@gmail.com>
2022-02-27 21:32:53 +0900

    안드로이드와 서버 연동

---
+++ android/app/src/main/java/com/sasarinomari/diary/APICall.kt
@@ -0,0 +1,115 @@
+package com.sasarinomari.diary
+
+import android.util.Log
+import com.google.gson.JsonObject
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+abstract class APICall {
+    abstract fun onError(message: String)
+    abstract fun onMessage(message: String)
+
+    private var token: String? = ""
+
+    // region Task API
+
+    fun getDays(callback: (Array<String>)->Unit) {
+        val call = APIInterface.api.getDays(token!!)
+        call.enqueue(object : Callback<Array<String>> {
+            override fun onResponse(call: Call<Array<String>>, response: Response<Array<String>>) {
+                if (response.isSuccessful) {
+                    val tasks = response.body()!!
+                    callback(tasks)
+                } else {
+                    onMessage("${response.code()} : ${response.message()}")
+                }
+            }
+
+            override fun onFailure(call: Call<Array<String>>, t: Throwable) {
+                onError(t.toString())
+            }
+        })
+    }
+
+    fun getDay(date: String, callback: (DiaryModel)->Unit) {
+        val task = DiaryModel()
+        task.date = date
+        val call = APIInterface.api.getDay(token!!, task)
+        call.enqueue(object : Callback<DiaryModel> {
+            override fun onResponse(call: Call<DiaryModel>, response: Response<DiaryModel>) {
+                if (response.isSuccessful) {
+                    val result = response.body()!!
+                    callback(result)
+                } else {
+                    onMessage("${response.code()} : ${response.message()}")
+                }
+            }
+
+            override fun onFailure(call: Call<DiaryModel>, t: Throwable) {
+                onError(t.toString())
+            }
+        })
+    }
+
+    fun createDay(task: DiaryModel, callback: (JsonObject)->Unit) {
+        val call = APIInterface.api.createDay(token!!, task)
+        call.enqueue(object : Callback<JsonObject> {
+            override fun onResponse(call: Call<JsonObject>, response: Response<JsonObject>) {
+                if (response.isSuccessful) {
+                    val result = response.body()!!
+                    Log.d("API_NEW_DAY", result.toString())
+                    callback(result)
+                } else {
+                    onMessage("${response.code()} : ${response.message()}")
+                }
+            }
+
+            override fun onFailure(call: Call<JsonObject>, t: Throwable) {
+                onError(t.toString())
+            }
+        })
+    }
+
+    // 변경할 부분만 Task Object에 넣으면 된다. 나머지 요소는 null
+    fun modifyDay(task: DiaryModel, callback: ()->Unit) {
+        val call = APIInterface.api.modifyDay(token!!, task)
+        call.enqueue(object : Callback<JsonObject> {
+            override fun onResponse(call: Call<JsonObject>, response: Response<JsonObject>) {
+                if (response.isSuccessful) {
+                    val result = response.body()!!
+                    Log.d("API_MODIFY_DAY", result.toString())
+                    callback()
+                } else {
+                    onMessage("${response.code()} : ${response.message()}")
+                }
+            }
+
+            override fun onFailure(call: Call<JsonObject>, t: Throwable) {
+                onError(t.toString())
+            }
+        })
+    }
+
+    fun deleteDay(id: Int, callback: ()->Unit) {
+        val task = DiaryModel()
+        task.idx = id
+        val call = APIInterface.api.deleteDay(token!!, task)
+        call.enqueue(object : Callback<JsonObject> {
+            override fun onResponse(call: Call<JsonObject>, response: Response<JsonObject>) {
+                if (response.isSuccessful) {
+                    val result = response.body()!!
+                    Log.d("API_REMOVE_DAY", result.toString())
+                    callback()
+                } else {
+                    onMessage("${response.code()} : ${response.message()}")
+                }
+            }
+
+            override fun onFailure(call: Call<JsonObject>, t: Throwable) {
+                onError(t.toString())
+            }
+
+        })
+    }
+}
---
+++ android/app/src/main/java/com/sasarinomari/diary/APIInterface.kt
@@ -0,0 +1,46 @@
+package com.sasarinomari.diary
+
+import com.google.gson.GsonBuilder
+import com.google.gson.JsonObject
+import okhttp3.OkHttpClient
+import retrofit2.Call
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.*
+
+interface APIInterface {
+    @GET("getDays")
+    fun getDays(@Header("key") token:String): Call<Array<String>>
+    @POST("getDay")
+    fun getDay(@Header("key") token:String, @Body body: DiaryModel): Call<DiaryModel>
+    @POST("createDay")
+    fun createDay(@Header("key") token:String, @Body body: DiaryModel): Call<JsonObject>
+    @POST("modifyDay")
+    fun modifyDay(@Header("key") token:String, @Body body: DiaryModel): Call<JsonObject>
+    @POST("deleteDay")
+    fun deleteDay(@Header("key") token:String, @Body body: DiaryModel): Call<JsonObject>
+
+    companion object {
+        private val BASE_URL = ""
+
+        private val gson = GsonBuilder()
+            .setLenient()
+            .create()
+
+        val okHttpClient = OkHttpClient
+            .Builder()
+            .build()
+
+        private val retrofit by lazy {
+            Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .client(okHttpClient)
+                .addConverterFactory(GsonConverterFactory.create(gson))
+                .build()
+        }
+
+        val api: APIInterface by lazy {
+            retrofit.create(APIInterface::class.java)
+        }
+    }
+}
