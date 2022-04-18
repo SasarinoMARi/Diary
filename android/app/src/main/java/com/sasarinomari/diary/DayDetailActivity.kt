8a2edf34501f86019aedfc896351165aaadfbfc1
Sasarino MARi <develop.mari@gmail.com>
2022-04-18 11:47:52 +0900

    ui 버그 및 삭제 버튼 추가

--- android/app/src/main/java/com/sasarinomari/diary/DayDetailActivity.kt
+++ android/app/src/main/java/com/sasarinomari/diary/DayDetailActivity.kt
@@ -1,15 +1,25 @@
 package com.sasarinomari.diary
 
+import android.app.AlertDialog
+import android.content.DialogInterface
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
-import androidx.activity.result.ActivityResultLauncher
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import com.google.gson.Gson
 import kotlinx.android.synthetic.main.activity_day_detail.*
 
+
 class DayDetailActivity : AppCompatActivity() {
+    private val api = object : APICall() {
+        override fun onError(message: String) {
+            Log.e("Error", message)
+        }
+    }
+
     private val conv = DateConverter()
     private val resultLauncher = registerForActivityResult(
         ActivityResultContracts.StartActivityForResult()) {
@@ -38,6 +48,22 @@ class DayDetailActivity : AppCompatActivity() {
             val i = Intent(this@DayDetailActivity, DayWriteActivity::class.java)
             i.putExtra("diary", Gson().toJson(diary))
             resultLauncher.launch(i)
+        }
+
+        button_delete.setOnClickListener {
+            val adb= AlertDialog.Builder(this)
+            adb.setTitle(getString(R.string.DeleteConfirmDialog))
+            adb.setPositiveButton(getString(R.string.OK)) { dialog, which ->
+                api.deleteDay(diary.idx) {
+                    Toast.makeText(this@DayDetailActivity, "일기를 삭제했습니다.", Toast.LENGTH_LONG).show()
+                    setResult(RESULT_OK)
+                    finish()
+                }
+            }
+            adb.setNegativeButton(getString(R.string.Cancel)) { dialog, which ->
+                //finish()
+            }
+            adb.show()
         }
     }
 }
